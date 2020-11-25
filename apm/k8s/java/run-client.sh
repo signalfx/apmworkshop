589d54b8cfda05a1d9470ece2a485cd400174d22
Steve L <slerner@splunk.com>
2020-11-24 23:40:20 -0500

    Update run-client.sh

--- apm/k8s/java/run-client.sh
+++ apm/k8s/java/run-client.sh
@@ -1,3 +1,5 @@
+export SIGNALFX_SERVICE_NAME='java-reqs-client'
+export SIGNALFX_ENDPOINT_URL='http://localhost:9080/v1/trace'
 mvn compile exec:exec \
   -Dexec.executable="java" \
   -Dexec.args="-javaagent:/opt/signalfx-tracing.jar -cp %classpath sf.main.GetExample"
