c08ce3323ba0bab2410e3ae7b5e08378018c8927
Ubuntu <slerner@splunk.com>
2020-11-30 16:57:35 -0500

    commit

--- apm/k8s/java/run-binary.sh
+++ apm/k8s/java/run-binary.sh
@@ -1,7 +1,7 @@
 curl -L https://github.com/signalfx/signalfx-java-tracing/releases/latest/download/signalfx-tracing.jar -o /opt/signalfx-tracing.jar
 chmod 755 /opt/signalfx-tracing.jar
 export SIGNALFX_SERVICE_NAME='k8s-java-reqs-client'
-export SIGNALFX_ENDPOINT_URL='http://localhost:9080/v1/trace'
+export SIGNALFX_ENDPOINT_URL='http://127.0.0.1:9080/v1/trace'
 java \
 -DtargetUrl=https://api.github.com \
 -javaagent:/opt/signalfx-tracing.jar \
