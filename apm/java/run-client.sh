b3e18300d3b5707c372a9e195999fb147100ddd9
Ubuntu <slerner@splunk.com>
2020-12-08 18:59:13 -0500

    commit

--- apm/java/run-client.sh
+++ apm/java/run-client.sh
@@ -1,3 +1,6 @@
+export OTEL_EXPORTER_JAEGER_SERVICE_NAME=java-otel-reqs-client
 mvn compile exec:exec \
-  -Dexec.executable="java" \
-  -Dexec.args="-javaagent:/opt/signalfx-tracing.jar -cp %classpath sf.main.GetExample"
+-Dexec.executable="java" \
+-Dotel.exporter=jaeger \
+-Dotel.exporter.jaeger.endpoint=http://127.0.0.1:9080/v1/trace \
+-Dexec.args="-javaagent:/opt/splunk-otel-javaagent.jar -cp %classpath sf.main.GetExample"
--- apm/java/setup.sh
+++
@@ -1,2 +0,0 @@
-sudo curl -L https://github.com/signalfx/signalfx-java-tracing/releases/latest/download/signalfx-tracing.jar -o /opt/signalfx-tracing.jar
-sudo chmod 755 /opt/signalfx-tracing.jar
