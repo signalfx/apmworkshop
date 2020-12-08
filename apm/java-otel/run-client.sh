5c0be4910f7447bb13f8a32f3d20755f0dd74f35
Ubuntu <slerner@splunk.com>
2020-12-08 08:55:04 -0500

    commit

--- apm/java-otel/run-client.sh
+++ apm/java-otel/run-client.sh
@@ -1,8 +1,9 @@
 sudo curl -L https://github.com/signalfx/splunk-otel-java/releases/latest/download/splunk-otel-javaagent-all.jar \
     -o /opt/splunk-otel-javaagent.jar
 sudo chmod 755 /opt/splunk-otel-javaagent.jar
-export SIGNALFX_SERVICE_NAME='java-otel-reqs-client'
-export SIGNALFX_ENDPOINT_URL='http://localhost:9080/v1/trace'
+export OTEL_EXPORTER=jaeger
+export OTEL_EXPORTER_JAEGER_SERVICE_NAME='java-otel-reqs-client'
+export OTEL_EXPORTER_JAEGER_ENDPOINT='http://localhost:9080/v1/trace'
 mvn compile exec:exec \
   -Dexec.executable="java" \
   -Dexec.args="-javaagent:/opt/splunk-otel-javaagent.jar -cp %classpath sf.main.GetExample"
