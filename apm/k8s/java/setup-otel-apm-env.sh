634cf4cb40443e11853b88ba136bc69af33d5eaf
Steve L <slerner@splunk.com>
2020-12-08 13:25:04 -0500

    Update setup-otel-apm-env.sh

--- apm/k8s/java/setup-otel-apm-env.sh
+++ apm/k8s/java/setup-otel-apm-env.sh
@@ -1,3 +1,3 @@
 export OTEL_EXPORTER=jaeger
-export OTEL_EXPORTER_JAEGER_SERVICE_NAME='java-otel-reqs-client'
-export OTEL_EXPORTER_JAEGER_ENDPOINT='http://localhost:9080/v1/trace'
+export OTEL_EXPORTER_JAEGER_SERVICE_NAME=java-otel-reqs-client
+export OTEL_EXPORTER_JAEGER_ENDPOINT=http://localhost:9080/v1/trace
