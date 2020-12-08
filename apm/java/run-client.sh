c562a440da38dc59a363571a3471986ef5f2ab81
Steve L <slerner@splunk.com>
2020-12-08 14:18:51 -0500

    Update run-client.sh

--- apm/java/run-client.sh
+++ apm/java/run-client.sh
@@ -2,5 +2,5 @@ mvn compile exec:exec \
 -Dexec.executable="java" \
 -Dotel.exporter.jaeger.service.name=java-otel-reqs-client \
 -Dotel.exporter=jaeger \
--Dotel.exporter.jaeger.endpoint=http://localhost:9080/v1/trace \
+-Dotel.exporter.jaeger.endpoint=http://127.0.0.1:9080/v1/trace \
 -Dexec.args="-javaagent:/opt/splunk-otel-javaagent.jar -cp %classpath sf.main.GetExample"
