1e2cae226bf779d746532f060f1e81828b3e34a8
Steve L <slerner@splunk.com>
2020-12-08 10:13:04 -0500

    Update run-binary.sh

--- apm/k8s/java/run-binary.sh
+++ apm/k8s/java/run-binary.sh
@@ -1,11 +1,11 @@
-curl -L https://github.com/signalfx/signalfx-java-tracing/releases/latest/download/signalfx-tracing.jar -o /opt/signalfx-tracing.jar
-chmod 755 /opt/signalfx-tracing.jar
-export SIGNALFX_SERVICE_NAME='k8s-java-reqs-client'
+curl -L https://github.com/signalfx/splunk-otel-java/releases/latest/download/splunk-otel-javaagent-all.jar -o /opt/splunk-otel-javaagent.jar
+chmod 755 /opt/splunk-otel-javaagent.jar
+export SIGNALFX_SERVICE_NAME='k8s-java-otel-reqs-client'
+export OTEL_EXPORTER=jaeger
 # change the env variable below to change location of your agent
 # export SIGNALFX_ENDPOINT_URL='YOURAGENTADDRESSHERE'
-
 # change target URL below to alter the test target
 java \
 -DtargetUrl=https://api.github.com \
--javaagent:/opt/signalfx-tracing.jar \
+-javaagent:/opt/splunk-otel-javaagent.jar \
 -jar ./target/java-app-1.0-SNAPSHOT.jar
