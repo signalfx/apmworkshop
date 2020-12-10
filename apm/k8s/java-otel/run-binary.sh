0b5fff1975b5c9ab10450a3f1b21bb7d98c1dc21
slerner@splunk.com <stevelerner@stevel-0472.fios-router.home>
2020-12-10 14:46:36 -0500

    commit

--- apm/k8s/java-otel/run-binary.sh
+++ apm/k8s/java-otel/run-binary.sh
@@ -1,9 +1,2 @@
-curl -L https://github.com/signalfx/splunk-otel-java/releases/latest/download/splunk-otel-javaagent-all.jar -o /opt/splunk-otel-javaagent.jar
-chmod 755 /opt/splunk-otel-javaagent.jar
-export OTEL_EXPORTER_JAEGER_SERVICE_NAME=java-otel-reqs-client
-# change the endoint env variable in the deployment .yaml to change location of your agent
-# change target URL below to alter the test target
-java \
--DtargetUrl=https://api.github.com \
--javaagent:/opt/splunk-otel-javaagent.jar \
--jar ./target/java-app-1.0-SNAPSHOT.jar
+sh ./install-java-otel.sh
+sh ./run-java.sh
