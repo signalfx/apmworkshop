05890b3e8b52ea90641dc88beedb01e307ad4125
Steve L <slerner@splunk.com>
2020-12-12 00:14:05 -0500

    Update run-binary.sh

--- apm/k8s/java/java-sfx/run-binary.sh
+++ apm/k8s/java/java-sfx/run-binary.sh
@@ -1,9 +1,5 @@
 curl -L https://github.com/signalfx/signalfx-java-tracing/releases/latest/download/signalfx-tracing.jar -o /opt/signalfx-tracing.jar
 chmod 755 /opt/signalfx-tracing.jar
-export SIGNALFX_SERVICE_NAME='k8s-java-reqs-client'
-# change the env variable below to change location of your agent
-# export SIGNALFX_ENDPOINT_URL='YOURAGENTADDRESSHERE'
-
 # change target URL below to alter the test target
 java \
 -DtargetUrl=https://api.github.com \
