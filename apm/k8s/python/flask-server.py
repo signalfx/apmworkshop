46c3913c047af32400a06fa2d747a6a74d51e886
Steve L <slerner@splunk.com>
2020-12-22 14:10:29 -0500

    Update flask-server.py

--- apm/k8s/python/flask-server.py
+++ apm/k8s/python/flask-server.py
@@ -3,13 +3,17 @@ from flask import Flask, make_response, request
 app = Flask(__name__)
    
 def convert_response(message):
-    return 'Hello From Flask Server\n'.format(message)
+    return format(message)
 
-@app.route('/echo', methods=['POST'])
-@app.route('/echo', methods=['GET'])
+@app.route('/echo', methods=['GET', 'POST'])
 def echo():
-    message = request.data.decode('utf-8')
-    return make_response(convert_response(message))
+    data = request.data
+    if request.method == 'POST':
+        print ('You posted ')
+        return(data)
+    else:
+        print ('You getted ')
+        return(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
