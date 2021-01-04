3b9e6f2a99d33aa2b8bab5c10a606cd94301ef9c
Ubuntu <slerner@splunk.com>
2021-01-04 15:57:09 -0500

    commit

--- apm/k8s/java/java-otel/run-java.sh
+++ apm/k8s/java/java-otel/run-java.sh
@@ -1,6 +1,6 @@
 # change the endoint env variable in the deployment .yaml to change location of your agent
 # change target URL below to alter the test target
 java \
--DtargetUrl=https://api.github.com \
+-DtargetUrl=http://localhost:5000/echo \
 -javaagent:/opt/splunk-otel-javaagent.jar \
 -jar ./java-otel/target/java-app-1.0-SNAPSHOT.jar
