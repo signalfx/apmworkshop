402e84897c6f3cd322d09ed83870e46eaa62ad19
Steve Lerner <slerner@splunk.com>
2021-01-16 00:41:15 -0500

    commit

--- apm/java-manual-inst/run-client.sh
+++
@@ -1,5 +0,0 @@
-mvn compile -X exec:exec \
--Dexec.executable="java" \
--Dotel.exporter.jaeger.endpoint=http://127.0.0.1:9080/v1/trace \
--Dotel.exporter.jaeger.service.name=java-otel-reqs-client \
--Dexec.args="-javaagent:/opt/splunk-otel-javaagent.jar -cp %classpath sf.main.GetExample"
