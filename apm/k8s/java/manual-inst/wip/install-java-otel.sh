8cbbd92c4e9db58f40cfe3a176d2d3af2341c52f
Ubuntu <slerner@splunk.com>
2021-01-20 00:22:04 -0500

    commit

---
+++ apm/k8s/java/manual-inst/wip/install-java-otel.sh
@@ -0,0 +1,3 @@
+curl -L https://github.com/signalfx/splunk-otel-java/releases/latest/download/splunk-otel-javaagent-all.jar \
+    -o /opt/splunk-otel-javaagent.jar
+chmod 755 /opt/splunk-otel-javaagent.jar
---
+++ apm/k8s/java/manual-inst/wip/src/main/java/sf/main/GetExample.java
@@ -0,0 +1,62 @@
+package sf.main;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.api.trace.attributes.SemanticAttributes;
+
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.context.Context;
+
+import java.util.Random; 
+import java.io.IOException;
+
+public class GetExample {
+
+private static final Tracer tracer =
+    OpenTelemetry.getTracer("io.opentelemetry.sf.main.GetExample");		
+
+private static void wait(int ms)
+{
+    try
+    {        Thread.sleep(ms);
+    }
+    catch(InterruptedException ex)
+    {        Thread.currentThread().interrupt();
+    }
+} // wait
+
+public static void main(String[] args) throws IOException {
+  int x = 1;
+
+  Random random = new Random();
+  int val = random.nextInt();
+  String userID = new String();
+  userID = Integer.toHexString(val);
+
+  while (x>0)
+    {
+       System.out.println(x);
+       x++;
+       wait(250);
+
+       // Start a span with scope
+       Span manualSpan = tracer.spanBuilder("manualSpan") // operation name
+	       .setSpanKind(Span.Kind.SERVER) // tag the span as a service boundary
+	       .startSpan();
+       try (Scope scope = manualSpan.makeCurrent()) {
+         // Add attributes
+         manualSpan.setAttribute("my.key", "myvalue");
+         manualSpan.setAttribute("user.ID", userID);
+         } finally {
+         // End span
+         manualSpan.end();
+	 } // finally
+      // End manual span stanza
+
+    } // while loop
+
+  } // main
+
+} // class
