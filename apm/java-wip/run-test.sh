67857970ed775208d33b8a3414d8d5ce935fea5a
Ubuntu <slerner@splunk.com>
2021-01-20 20:13:21 -0500

    commit

--- apm/java-wip/run-test.sh
+++ apm/java-wip/run-test.sh
@@ -2,6 +2,5 @@ sh ./manual-inst/install-java-otel.sh
 # change target URL below to alter the test target
 java \
 -javaagent:/opt/splunk-otel-javaagent.jar \
--Dotel.resource.attibutes=service.name=GetExampleService \
--Dotel.exporter=logging \
+-Dotel.resource.attributes=service.name=GetExampleService \
 -jar ./target/java-app-1.0-SNAPSHOT.jar
