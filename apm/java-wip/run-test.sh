8a1cc559db9281afe3098b72cbadcc853771adc1
jkwatson <jkwatson@gmail.com>
2021-01-20 16:51:03 -0800

    Update the GetExample for 0.14.1 and add an annotated inner span.

--- apm/java-wip/run-test.sh
+++ apm/java-wip/run-test.sh
@@ -2,4 +2,6 @@ sh ./manual-inst/install-java-otel.sh
 # change target URL below to alter the test target
 java \
 -javaagent:/opt/splunk-otel-javaagent.jar \
+-Dotel.resource.attibutes=service.name=GetExampleService \
+-Dotel.exporter=logging \
 -jar ./target/java-app-1.0-SNAPSHOT.jar
--- apm/java-wip/src/main/java/sf/main/GetExample.java
+++ apm/java-wip/src/main/java/sf/main/GetExample.java
@@ -1,50 +1,58 @@
 package sf.main;
 
-import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
-
-import java.io.IOException;
+import io.opentelemetry.extension.annotations.WithSpan;
 
 public class GetExample {
-	
-  // Instantiate a tracer
- private static final Tracer tracer =
-      OpenTelemetry.getGlobalTracer("io.opentelemetry.sf.main.GetExample");
 
-public static void wait(int ms)
-{
-    try
-    {        Thread.sleep(ms);
-    }
-    catch(InterruptedException ex)
-    {        Thread.currentThread().interrupt();
-    }
-} // wait
-	
-public static void main(String[] args) throws IOException {
-  int x = 1;
-  while (x < 0)
-  {
-	  
-    System.out.println(x);
-    x++;
-    wait(250);
-    // Start a span with scope
-     Span exampleSpan = tracer.spanBuilder("exampleSpan") // operation name
-	     .setSpanKind(Span.Kind.SERVER) // tag the span as a service boundary
-	     .startSpan();
-       try (Scope scope = exampleSpan.makeCurrent()) {
-         // Add attributes
-         exampleSpan.setAttribute("my.key", "myvalue");
-         exampleSpan.setAttribute("service.name", "someotherservice");
-	 exampleSpan.addEvent("MyEvent");
-       } finally {
-         // End span
-         exampleSpan.end();
-	     } // finally
-      // End manual span stanza
+  // Instantiate a tracer. Since we're using the opentelemetry auto-instrumentation agent,
+  // we can be sure that the agent will have set up the SDK for us and it should be ready to use.
+  private static final Tracer tracer =
+      GlobalOpenTelemetry.getTracer("io.opentelemetry.sf.main.GetExample");
+
+  public static void main(String[] args) {
+    int x = 1;
+    while (x < Integer.MAX_VALUE) {
+      System.out.println(x);
+      x++;
+      fullyManualInstrumentation();
     } // while loop
   } // main
-} // class
+
+  private static void wait(int ms) {
+    try {
+      Thread.sleep(ms);
+    } catch (InterruptedException ex) {
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  private static void fullyManualInstrumentation() {
+    Span exampleSpan = tracer.spanBuilder("exampleSpan") // operation name
+        .setSpanKind(Span.Kind.SERVER) // tag the span as a service boundary
+        .startSpan();
+    // Put the span into the current context
+    try (Scope scope = exampleSpan.makeCurrent()) {
+      // Add attributes
+      exampleSpan.setAttribute("my.key", "myvalue");
+      exampleSpan.setAttribute("service.name", "someotherservice");
+      exampleSpan.addEvent("MyEvent");
+      //simulate some work
+      doSomeWork();
+    } finally {
+      // Always end the span in a finally block.
+      exampleSpan.end();
+    }
+  }
+
+  //the agent will automatically create a span for you and put it into the current Context.
+  @WithSpan
+  private static void doSomeWork() {
+    Span.current().addEvent("starting sleep");
+    wait(250);
+    Span.current().addEvent("stopping sleep");
+  }
+}
