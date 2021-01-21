b8dc55c2886875fd40465a8150e3f6fcfd878ace
Steve L <slerner@splunk.com>
2021-01-21 08:52:08 -0500

    Update install-java-otel.sh

--- apm/java/install-java-otel.sh
+++ apm/java/install-java-otel.sh
@@ -1,3 +1,3 @@
-sudo curl https://github.com/signalfx/splunk-otel-java/releases/latest/download/splunk-otel-javaagent-all.jar \
+sudo curl -L https://github.com/signalfx/splunk-otel-java/releases/latest/download/splunk-otel-javaagent-all.jar \
     -o /opt/splunk-otel-javaagent.jar
 sudo chmod 755 /opt/splunk-otel-javaagent.jar
