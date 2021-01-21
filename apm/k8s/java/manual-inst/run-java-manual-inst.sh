c0ad39a91dfed67fc16c389ebc224e468f755742
Ubuntu <slerner@splunk.com>
2021-01-20 20:48:25 -0500

    commit

--- apm/k8s/java/manual-inst/run-java-manual-inst.sh
+++ apm/k8s/java/manual-inst/run-java-manual-inst.sh
@@ -1,6 +1,5 @@
 sh ./manual-inst/install-java-otel.sh
 # change target URL below to alter the test target
 java \
--DtargetUrl=http://$SERVER_FLASK_OTEL_SERVICE_HOST:$SERVER_FLASK_OTEL_SERVICE_PORT/echo?key=value \
 -javaagent:/opt/splunk-otel-javaagent.jar \
 -jar ./manual-inst/target/java-app-1.0-SNAPSHOT.jar
