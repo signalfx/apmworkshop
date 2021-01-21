9d5fb2fe57aa55270372993ad85c3cfb5ed8b34b
Ubuntu <slerner@splunk.com>
2021-01-20 20:42:17 -0500

    commit

--- apm/java/java-manual-inst/run-test.sh
+++ apm/java/java-manual-inst/run-test.sh
@@ -3,4 +3,5 @@ sh ./manual-inst/install-java-otel.sh
 java \
 -javaagent:/opt/splunk-otel-javaagent.jar \
 -Dotel.resource.attributes=service.name=GetExampleService \
+-Dotel.exporter.jaeger.service.name=GetExampleService \
 -jar ./target/java-app-1.0-SNAPSHOT.jar
