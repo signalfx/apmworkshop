fdc9296a5ab568068a4daa908b80b55095ec250f
Steve L <slerner@splunk.com>
2021-01-20 19:21:26 -0500

    Update run-test.sh

--- apm/java-wip/run-test.sh
+++ apm/java-wip/run-test.sh
@@ -1,6 +1,5 @@
 sh ./manual-inst/install-java-otel.sh
 # change target URL below to alter the test target
 java \
--DtargetUrl=http://localhost:5000/echo?key=value \
 -javaagent:/opt/splunk-otel-javaagent.jar \
 -jar ./target/java-app-1.0-SNAPSHOT.jar
