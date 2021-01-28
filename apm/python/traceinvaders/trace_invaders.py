a5220e71909b0130146a635e40a22b057a29beb5
Ubuntu <slerner@splunk.com>
2021-01-28 14:44:43 -0500

    commit

--- apm/python/traceinvaders/trace_invaders.py
+++ apm/python/traceinvaders/trace_invaders.py
@@ -1,5 +1,4 @@
 # space.py
-	
 
 import turtle
 import math
@@ -14,41 +13,40 @@ tracer = trace.get_tracer(__name__)
 
 # Move player to the right
 def move_right():
-	x = player.xcor()
-	x = x + playerspeed
-	if x > 280:
-		x = 280
-	player.setx(x)
+    x = player.xcor()
+    x = x + playerspeed
+    if x > 280:
+        x = 280
+    player.setx(x)
 
 def move_left():
-	x = player.xcor()
-	x = x - playerspeed
-	if x < -280:
-		x = -280
-	player.setx(x)
+    x = player.xcor()
+    x = x - playerspeed
+    if x < -280:
+        x = -280
+    player.setx(x)
 
 def leave_game():
-	exit()
+    exit()
 
 def fire_bullet():
-	# Declare bulletstate as global variable
-	global bulletstate
-	if bulletstate == "ready":
-		os.system("afplay laser.wav&")
-		bulletstate = "fire"
-		# Move bullet to just above the player
-		x = player.xcor()
-		y = player.ycor() + 10
-		bullet.goto(x, y)
-		bullet.showturtle()
+    # Declare bulletstate as global variable
+    global bulletstate
+    if bulletstate == "ready":
+        os.system("afplay laser.wav&")
+        bulletstate = "fire"
+        # Move bullet to just above the player
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.goto(x, y)
+        bullet.showturtle()
 
 def is_collision(obj1, obj2):
-	distance = math.sqrt((obj1.xcor() - obj2.xcor()) ** 2 + (obj1.ycor() - obj2.ycor()) ** 2)
-	
-	if distance < 15:
-		return True
-	else:
-		return False
+    distance = math.sqrt((obj1.xcor() - obj2.xcor()) ** 2 + (obj1.ycor() - obj2.ycor()) ** 2)
+    if distance < 15:
+        return True
+    else:
+        return False
 
 # Set up the screen
 wn = turtle.Screen()
@@ -71,8 +69,8 @@ border_pen.up()
 border_pen.goto(-300, -300)
 border_pen.down()
 for side in range(4):
-	border_pen.forward(600)
-	border_pen.left(90)
+    border_pen.forward(600)
+    border_pen.left(90)
 
 # Set score to zero
 score = 0
@@ -106,17 +104,17 @@ enemies = []
 
 # Add enemies to list
 for i in range(number_of_enemies):
-	# Create enemy
-	enemies.append(turtle.Turtle())
+    # Create enemy
+    enemies.append(turtle.Turtle())
 
 for enemy in enemies:
-	enemy.color("red")
-	enemy.shape("invader.gif")
-	enemy.speed(0)
-	enemy.up()
-	x = random.randint(-200, 200)
-	y = random.randint(100, 250)
-	enemy.goto(x, y)
+    enemy.color("red")
+    enemy.shape("invader.gif")
+    enemy.speed(0)
+    enemy.up()
+    x = random.randint(-200, 200)
+    y = random.randint(100, 250)
+    enemy.goto(x, y)
 
 enemyspeed = 2
 
@@ -148,76 +146,77 @@ turtle.onkey(leave_game, "q")
 # Main Game Loop
 while True:
 
-	for enemy in enemies:
-			
-		# Move the enemy
-		x = enemy.xcor()
-		x = x + enemyspeed
-		enemy.setx(x)
+    for enemy in enemies:
 
-		# Move enemy back and down
-		if enemy.xcor() > 280:
-			for e in enemies:
-				y = e.ycor()
-				y = y - 40
-				e.sety(max(y, -250))		# Keep enemy from going below the level of the player
-			enemyspeed = enemyspeed * -1
-			with tracer.start_as_current_span("AlienMovement"):
-				current_span = trace.get_current_span()
-				current_span.set_attribute("span.kind", "SERVER")
-				current_span.set_attribute("error", "true")
-			# end parent span
+        # Move the enemy
+        x = enemy.xcor()
+        x = x + enemyspeed
+        enemy.setx(x)
 
-		if enemy.xcor() < -280:
-			for e in enemies:
-				y = e.ycor()
-				y = y - 40
-				e.sety(max(y, -250))		# Keep enemy from going below the level of the player
-			enemyspeed = enemyspeed * -1
-			with tracer.start_as_current_span("AlienMovement"):
-				current_span = trace.get_current_span()
-				current_span.set_attribute("span.kind", "SERVER")
-				current_span.set_attribute("error", "true")
-			# end parent span
+        # Move enemy back and down
+        if enemy.xcor() > 280:
+            for e in enemies:
+                y = e.ycor()
+                y = y - 40
+                e.sety(max(y, -250))        # Keep enemy from going below the level of the player
+            enemyspeed = enemyspeed * -1
+            # with tracer create a span
+            with tracer.start_as_current_span("AlienMovement"):
+                current_span = trace.get_current_span()
+                current_span.set_attribute("span.kind", "SERVER")
+                current_span.set_attribute("error", "true")
 
-		# Check for collision between bullet and enemy
-		if is_collision(bullet, enemy):
-			os.system("afplay explosion.wav&")
-			# Reset the bullet
-			bullet.hideturtle()
-			bulletstate = "ready"
-			bullet.goto(0, -400)
-			# Reset the enemy
-			x = random.randint(-200, 200)
-			y = random.randint(100, 250)
-			enemy.goto(x, y)
-			# Update the score
-			score += 10
-			score_string = "Score: {}".format(score)
-			score_pen.clear()
-			score_pen.write(score_string, False, align="left", font=("Arial", 14, "normal"))
-			with tracer.start_as_current_span("AlienHit"):
-				current_span = trace.get_current_span()
-				current_span.set_attribute("span.kind", "SERVER")
+        if enemy.xcor() < -280:
+            for e in enemies:
+                y = e.ycor()
+                y = y - 40
+                e.sety(max(y, -250))        # Keep enemy from going below the level of the player
+            enemyspeed = enemyspeed * -1
+            # with tracer create a span
+            with tracer.start_as_current_span("AlienMovement"):
+                current_span = trace.get_current_span()
+                current_span.set_attribute("span.kind", "SERVER")
+                current_span.set_attribute("error", "true")
 
-		# Check for collision between enemy and player
-		if is_collision(enemy, player):
-			player.hideturtle()
-			enemy.hideturtle()
-			enemy.goto(-100, 0)
-			enemy.write("Game Over!!", font=("Helvetica", 36, "normal"))
-			print("Game Over")
-			break
+        # Check for collision between bullet and enemy
+        if is_collision(bullet, enemy):
+            os.system("afplay explosion.wav&")
+            # Reset the bullet
+            bullet.hideturtle()
+            bulletstate = "ready"
+            bullet.goto(0, -400)
+            # Reset the enemy
+            x = random.randint(-200, 200)
+            y = random.randint(100, 250)
+            enemy.goto(x, y)
+            # Update the score
+            score += 10
+            score_string = "Score: {}".format(score)
+            score_pen.clear()
+            score_pen.write(score_string, False, align="left", font=("Arial", 14, "normal"))
+            # with tracer create a span
+            with tracer.start_as_current_span("AlienHit"):
+                current_span = trace.get_current_span()
+                current_span.set_attribute("span.kind", "SERVER")
 
-	# Move the bullet
-	if bulletstate == "fire":
-		y = bullet.ycor()
-		y = y + bulletspeed
-		bullet.sety(y)
+        # Check for collision between enemy and player
+        if is_collision(enemy, player):
+            player.hideturtle()
+            enemy.hideturtle()
+            enemy.goto(-100, 0)
+            enemy.write("Game Over!!", font=("Helvetica", 36, "normal"))
+            print("Game Over")
+            break
 
-	# Check if bullet has gone to the top
-	if bullet.ycor() > 280:
-		bullet.hideturtle()
-		bulletstate = "ready"
+    # Move the bullet
+    if bulletstate == "fire":
+        y = bullet.ycor()
+        y = y + bulletspeed
+        bullet.sety(y)
+
+    # Check if bullet has gone to the top
+    if bullet.ycor() > 280:
+        bullet.hideturtle()
+        bulletstate = "ready"
 
 delay = input("Press ENTER to end.")
