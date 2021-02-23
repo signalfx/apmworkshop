6a93bd5bf7e3975d0d4e2e9655d5760ba749d356
Ubuntu <slerner@splunk.com>
2021-02-23 16:12:11 -0500

    commit

--- apm/java/run-client.sh
+++ apm/java/run-client.sh
@@ -1,7 +1,6 @@
-export OTEL_EXPORTER_JAEGER_SERVICE_NAME=java-otel-reqs-client
 java \
--Dexec.executable="java" \
--Dotel.exporter.jaeger.endpoint=http://127.0.0.1:9080/v1/trace \
--Dotel.exporter.jaeger.service.name=java-otel-reqs-client \
--javaagent:/opt/splunk-otel-javaagent.jar \
--jar ./target/java-app-1.0-SNAPSHOT.jar
+	-Dexec.executable="java" \
+	-Dotel.exporter.jaeger.endpoint=http://127.0.0.1:9080/v1/trace \
+	-Dotel.resource.attributes=service.name=java-otel-reqs-client \
+	-javaagent:/opt/splunk-otel-javaagent.jar \
+	-jar ./target/java-app-1.0-SNAPSHOT.jar
