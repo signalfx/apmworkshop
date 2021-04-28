734aa0d24f1b8b68cf31724f688403d6eada54a1
Steve Lerner <stevelerner@gmail.com>
2021-04-27 22:10:02 -0400

    commit

--- apm/k8s/python/tools/delete-autogen.sh
+++ apm/k8s/python/tools/delete-autogen.sh
@@ -1,2 +1,2 @@
-kubectl delete deployment python-autogen-otel
-kubectl delete service python-autogen-otel
+kubectl delete deployment pod-py-autogen-otel
+kubectl delete service pod-py-autogen-otel
--- apm/k8s/python/tools/python-redis.py
+++ apm/k8s/python/tools/python-redis.py
@@ -1,6 +1,7 @@
 import redis
 from time import sleep
 from random import random, seed
+import datetime
 
 redis_host = "127.0.0.1"
 redis_port = 6379
@@ -13,13 +14,15 @@ def hello_redis():
     try:
         r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
         r.set("msg:hello", "Hello Redis!!!")
-        msg = r.get("msg:hello")
-        print(msg)
+#       msg = r.get("msg:hello")
+#       print(msg)
     except Exception as e:
-        print(e)
+#       print(e)
 
 while True:
+    now = datetime.datetime.now()
     hello_redis()
     y=round(random(),1)+.25
-    print('Sleeping: ', y)
     sleep(y)
+    print (now.strftime("%Y-%m-%d %H:%M:%S"))
+#   print('Sleeping: ', y)
