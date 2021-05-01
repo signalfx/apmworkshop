2af7fb77037488cfed4e0aa6195438037083826d
Ubuntu <slerner@splunk.com>
2021-05-01 13:37:49 +0000

    commit

--- apm/k8s/python/tools/python-redis.py
+++ apm/k8s/python/tools/python-redis.py
@@ -3,6 +3,7 @@ from time import sleep
 from random import random, seed, randint
 import datetime
 from opentelemetry import trace
+import json
 
 redis_host = "127.0.0.1"
 redis_port = 6379
@@ -28,13 +29,15 @@ while True:
     hex_number = str(hex(random_number))
     hex_number = hex_number[2:]
     printtime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]+"Z"
+    log_dict = {'transactionTime': printtime,
+                'transactionID': hex_number
+                }
     
     tracer = trace.get_tracer(__name__)     # create a manual span for a logging operation called "log"
     with tracer.start_as_current_span("log") as span:
-        print(printtime, " transactionID ", hex_number)
         span.set_attribute("transactionTime", printtime)
         span.set_attribute("transactionID", hex_number)
-        span.set_attribute("Span.Kind", "SERVER") # APM app type is "service"
+        print(json.dumps(log_dict))
     
     y=round(random(),1)+.25
     sleep(y)
