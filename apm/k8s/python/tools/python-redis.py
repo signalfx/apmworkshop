d0416a3ac6aa9694e498ce3a5b1beb544e0fc3d3
Ubuntu <slerner@splunk.com>
2021-05-01 03:42:51 +0000

    commit

--- apm/k8s/python/tools/python-redis.py
+++ apm/k8s/python/tools/python-redis.py
@@ -11,7 +11,7 @@ redis_password = ""
 seed(1)
 x=1
 
-def hello_redis():
+def hello_redis():  # simple redis example that will be picked up by auto-instrumentation
     try:
         r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
         r.set("msg:hello", "Hello Redis!!!")
@@ -21,21 +21,21 @@ def hello_redis():
         print(e)
 
 while True:
+
+    hello_redis()   # redis is picked up by auto-instrumentation   
+
     random_number = randint(0,16777215)
     hex_number = str(hex(random_number))
     hex_number = hex_number[2:]
-
-    hello_redis()
-
-    y=round(random(),1)+.25
-    sleep(y)
     printtime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]+"Z"
-
-    tracer = trace.get_tracer(__name__)
-
-    with tracer.start_as_current_span("print") as span:
+    
+    tracer = trace.get_tracer(__name__)     # create a manual span for a logging operation called "log"
+    with tracer.start_as_current_span("log") as span:
         print(printtime, " transactionID ", hex_number)
         span.set_attribute("transactionTime", printtime)
         span.set_attribute("transactionID", hex_number)
-
+        span.set_attribute("span.kind", "server") # APM app type is "service"
+    
+    y=round(random(),1)+.25
+        sleep(y)
 #   print('Sleeping: ', y)
