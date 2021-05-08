b6c36faf0912c2316679b429998b34359240f0db
Ubuntu <slerner@splunk.com>
2021-05-08 23:15:45 +0000

    commit

--- apm/python/run-client.sh
+++ apm/python/run-client.sh
@@ -1,7 +1,6 @@
 # set up OTEL env variables
-export OTEL_RESOURCE_ATTRIBUTES=service.name=py-otel-reqs-client
+export OTEL_RESOURCE_ATTRIBUTES=service.name=py-otel-reqs-client,deployment.environment=apm-workshop
 export OTEL_EXPORTER_JAEGER_ENDPOINT=http://localhost:9080/v1/trace
-export OTEL_RESOURCE_ATTRIBUTES=deployment.environment=apm-workshop
 export SPLUNK_SERVICE_NAME=py-otel-client
 # ensure path is correct
 export PATH="$HOME/.local/bin:$PATH"
--- apm/python/run-server.sh
+++ apm/python/run-server.sh
@@ -1,7 +1,6 @@
 # set up OTEL env variables
-export OTEL_RESOURCE_ATTRIBUTES=service.name=py-otel-flask-server
+export OTEL_RESOURCE_ATTRIBUTES=service.name=py-otel-flask-server,deployment.environment=apm-workshop
 export OTEL_EXPORTER_JAEGER_ENDPOINT=http://localhost:9080/v1/trace
-export OTEL_RESOURCE_ATTRIBUTES=deployment.environment=apm-workshop
 export SPLUNK_SERVICE_NAME=py-otel-flask-server
 # ensure path is correct
 export PATH="$HOME/.local/bin:$PATH"
