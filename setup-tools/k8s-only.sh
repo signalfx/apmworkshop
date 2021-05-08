8b6234d341c2f758eafc2b8c4f235556108f9bfb
Ubuntu <slerner@splunk.com>
2021-05-08 16:18:38 +0000

    commit

--- setup-tools/k8s-only.sh
+++ setup-tools/k8s-only.sh
@@ -5,11 +5,13 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 #install helm
 sudo snap install helm --classic
-helm repo add signalfx https://dl.signalfx.com/helm-repo
+helm repo add splunk-otel-collector-chart https://signalfx.github.io/splunk-otel-collector-chart
 helm repo update
 
 #clone workshop
 git clone https://github.com/signalfx/apmworkshop
 
 #update .bashrc for workshop
+echo -e "\n\n" >> /home/ubuntu/.bashrc
 cat bashrc >> /home/ubuntu/.bashrc
+source /home/ubuntu/.bashrc
