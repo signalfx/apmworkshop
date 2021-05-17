65097970625dfc4e5e7ea39e622d6d12e1cb1a95
Ubuntu <ubuntu@ip-172-31-35-223.ec2.internal>
2021-05-17 22:39:47 +0000

    commit

--- apm/python/run-server.sh
+++ apm/python/run-server.sh
@@ -2,6 +2,10 @@
 export OTEL_RESOURCE_ATTRIBUTES=service.name=py-otel-flask-server,deployment.environment=apm-workshop
 export OTEL_EXPORTER_JAEGER_ENDPOINT=http://localhost:9080/v1/trace
 export OTEL_SERVICE_NAME=py-otel-flask-server
+export OTEL_TRACES_EXPORTER=jaeger-thrift-splunk
+export OTEL_PROPAGATORS=b3multi
+#   - name: OTEL_EXPORTER_OTLP_ENDPOINT
+#     value: http://$(SPLUNK_OTEL_AGENT):4317
 # ensure path is correct
 export PATH="$HOME/.local/bin:$PATH"
 splk-py-trace python3 flask-server.py
