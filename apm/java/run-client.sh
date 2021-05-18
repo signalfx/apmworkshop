ba5ad9fc9d3cc34db6d28c372cac250505621f7f
Ubuntu <ubuntu@ip-172-31-92-181.ec2.internal>
2021-05-18 00:18:26 +0000

    commit

--- apm/java/run-client.sh
+++ apm/java/run-client.sh
@@ -1,6 +1,8 @@
 java \
 -Dexec.executable="java" \
 -Dotel.exporter.jaeger.endpoint=http://127.0.0.1:9080/v1/trace \
+-Dotel.propagators=b3multi \
+-Dotel.traces.exporter=jaeger-thrift-splunk \
 -Dotel.resource.attributes=service.name=java-otel-client,deployment.environment=apm-workshop \
 -javaagent:/opt/splunk-otel-javaagent.jar \
 -jar ./target/java-app-1.0-SNAPSHOT.jar
