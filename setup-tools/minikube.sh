d3de5279af521f159cbf0e56a28890a82520446d
Steve L <slerner@splunk.com>
2021-06-30 19:34:27 -0400

    Update minikube.sh

--- setup-tools/minikube.sh
+++ setup-tools/minikube.sh
@@ -8,7 +8,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER && newgrp docker
+sudo usermod -aG docker $USER
+newgrp docker
 sudo snap install kubectl --classic
 
 curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
