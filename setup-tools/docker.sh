65753afca8a8880f3b7bafaca8ebce1918157363
Steve L <slerner@splunk.com>
2021-10-13 20:32:42 -0400

    Delete setup-tools directory

--- setup-tools/docker.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get -y update
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER && newgrp docker
--- setup-tools/remove-k3s.sh
+++
@@ -1,1 +0,0 @@
-/usr/local/bin/k3s-uninstall.sh
